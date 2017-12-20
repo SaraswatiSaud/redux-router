e04594e6e450e9ed133a3a9dbaec3525b267a9d5
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-20 11:20:37 +0545

    Created basic path of react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+// BrowserRouter interacts with history library and decides what to show on screen
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Hello extends React.Component {
+  render(){
+    return(<div>Hello</div>)
+  }
+}
+
+class Goodbye extends React.Component {
+  render(){
+    return(<div>Goodbye</div>)
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      Header
+      <Route path='/hello' component={Hello} />
+      <Route path='/goodbye' component={Goodbye} />
+    </div>
+  </BrowserRouter>
+, document.getElementById('root'));
